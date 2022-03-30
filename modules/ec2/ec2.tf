4637f629f1d00149d50e1477d8ade4edc36dcef4
Cloudecoder <70545534+Cloudecoder@users.noreply.github.com>
2022-03-30 15:06:46 +0100

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -37,4 +37,37 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = var.sg_id
 }
 
-variable "sg_id" {}
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow Ssh inbound traffic"
+
+
+  ingress {
+    description      = "ssh connection"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+module "log_group" {
+  source  = "terraform-aws-modules/cloudwatch/aws//modules/log-group"
+  version = "~> 2.0"
+
+  name              = "cloud_watch"
+  retention_in_days = 120
+}
